eb12f496969ff90825ec3b98992e3a6596095383
huyBach1609 <bachlh03@gmail.com>
2025-08-01 12:08:40 +0700

    change mockurl

--- SAPS.Web/src/config/base.tsx
+++ SAPS.Web/src/config/base.tsx
@@ -2,7 +2,7 @@ export const mockUrl =
   "https://a12fd66f-74be-4779-97a3-4fef023e9291.mock.pstmn.io";
 export const localUrl = "https://localhost:7136";
 export const localUrl2 = "https://localhost:7284";
-export const apiUrl = localUrl2;
+export const apiUrl = localUrl;
 export const OWNER_ROLE = "parkinglotowner";
 
 export const ADMIN_ROLE = "admin";
