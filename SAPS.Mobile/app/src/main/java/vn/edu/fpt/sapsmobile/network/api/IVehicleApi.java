007ecb2476996a4a11bcf6db9536da5f36f5134a
weback1609 <bachlh03@gmail.com>
2025-09-13 21:13:47 +0700

    remove unuse method

--- SAPS.Mobile/app/src/main/java/vn/edu/fpt/sapsmobile/network/api/IVehicleApi.java
+++ SAPS.Mobile/app/src/main/java/vn/edu/fpt/sapsmobile/network/api/IVehicleApi.java
@@ -10,15 +10,6 @@ import vn.edu.fpt.sapsmobile.models.Vehicle;
 import vn.edu.fpt.sapsmobile.dtos.vehicle.VehicleSummaryDto;
 
 public interface IVehicleApi {
-    // mock api
-    @GET("/vehicle") // endpoint bạn cần gọi
-    Call<List<Vehicle>> getListVehicles();
-    @GET("/vehicle/{vehicleID}") // endpoint bạn cần gọi
-    Call<Vehicle> getVehiclebyID(@Path("vehicleID") String vehicleID);
-    @GET("/vehicleShared") // endpoint bạn cần gọi
-    Call<List<Vehicle>> getMySharedVehicles();
-
-    // dev
     @GET("/api/vehicle/my-vehicles")
     Call<List<VehicleSummaryDto>> getMyVehicles(
         @Query("status") String status,
